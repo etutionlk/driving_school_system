80190cd30c8026a7fed56d8849c34ddfb213e424
Chinthaka Maduranga <etutionlk@gmail.com>
2023-04-09 22:53:48 +0530

    created flask app

---
+++ models.py
@@ -0,0 +1,12 @@
+"""
+Name : models.py
+Author : Chinthaka Maduranga
+Contact : chinthaka.maduranga@blackswan-technologies.com
+Time : 09/04/2023 6:48 PM
+Desc: models
+"""
+from app import db
+
+
+class Candidate(db.Models):
+    pass
