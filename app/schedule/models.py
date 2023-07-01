0fab1090e3d95000c11885dedc69025ee0a32d59
Chinthaka Maduranga <etutionlk@gmail.com>
2023-07-01 08:45:36 +0530

    phase #1 model relationships are created

--- app/schedule/models.py
+++ app/schedule/models.py
@@ -12,7 +12,7 @@ from app.util import LessonStatus
 class LessonSchedule(db.Model):
     __tablename__ = "lesson_schedule"
     lesson_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    candidate_id = db.Column(db.Integer, db.ForeignKey("candidate.candidate_id", onupdate="CASCADE", ondelete="CASCADE"))
+    candidate_id = db.Column(db.Integer, db.ForeignKey("candidate.candidate_id"))
     licence_category_id = db.Column(db.Integer, db.ForeignKey("licence_category.licence_category_id"))
     instructor_id = db.Column(db.Integer, db.ForeignKey("instructor.instructor_id"))
     vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicle.vehicle_id"))
